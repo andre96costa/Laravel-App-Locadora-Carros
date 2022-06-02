61bd415bfe7946b080f25bcf2ae5bdd69221746b
Andre Costa <andre2396costa@gmail.com>
2022-06-02 20:23:58 -0300

    Recurso cliente

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -3,62 +3,114 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cliente;
+use App\Repositories\ClienteRepository;
 use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
+    private $clienteModel;
+    
+    public function __construct(Cliente $clienteModel) {
+        $this->clienteModel = $clienteModel;
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        //$clienteRepository = new ClienteRepository($this->clienteModel);
+        
+        // if ($request->has('atributos_modelo')) {
+        //     $atributosModelo = "modelo:$request->atributos_modelo";
+        //     $carroRepository->selectAtributesRegistroRelacionamento($atributosModelo);
+        // } else {
+        //     $carroRepository->selectAtributesRegistroRelacionamento('modelo');
+        // }
+
+        // if ($request->has('filtro')) {
+        //     $carroRepository->filtro($request->filtro);
+        // }
+
+        // if ($request->has('atributos')) {
+        //     $carroRepository->selectAtributes($request->atributos);
+        // }
+        
+        return response()->json($this->clienteModel->all(), 200);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->clienteModel->rules(), $this->clienteModel->feedback());
+        $cliente = $this->clienteModel->create([
+            'nome' => $request->nome,
+        ]);
+        return response()->json($cliente, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function show(Cliente $cliente)
+    public function show(int $id)
     {
-        //
+        $cliente = $this->clienteModel->find($id);
+        if (empty($cliente)) {
+            return response()->json([], 404);
+        }
+        return response()->json($cliente, 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  Illuminate\Http\Request  $request
-     * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Cliente $cliente)
+    public function update(Request $request, int $id)
     {
-        //
+        $cliente = $this->clienteModel->find($id);
+        if (empty($cliente)) {
+            return response()->json([], 404);
+        }
+
+        if ($request->method() === 'PUT') {
+            $request->validate($this->clienteModel->rules(), $this->clienteModel->feedback());
+        }
+
+        if ($request->method() === 'PATCH') {
+            $regrasDinamicas = array();
+            foreach ($this->clienteModel->rules() as $key => $value) {
+                if (array_key_exists($key, $request->all())) {
+                    $regrasDinamicas[$key] = $value;
+                }
+            }
+            $request->validate($regrasDinamicas, $this->clienteModel->feedback());
+        }
+        $cliente->fill($request->all());
+        $cliente->save();
+        return response()->json($cliente, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Cliente $cliente)
+    public function destroy(int $id)
     {
-        //
+        $cliente = $this->clienteModel->find($id);
+        if (empty($cliente)) {
+            return response()->json([], 404);
+        }
+        $cliente->delete();
+        return response()->json([], 204);
     }
 }
--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nome',
+    ];
+
+    public function rules()
+    {
+        return [
+            'nome' => "required|min:3|max:50",
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.min' => 'O nome deve ter no minimo 3 carecteres',
+            'nome.max' => 'O nome deve ter no maximo 50 carecteres',
+        ];
+    }
 }
